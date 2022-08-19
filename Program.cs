64f5000cc1448aff5b72bf2907b2e2647e462ca7
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-08-19 10:52:49 -0300

    Ajustes

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿using BlazorStrap;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using wasmSample;
-using wasmSample.Shared;
-using BlazorStrap;
+using wasmSample.componentes;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -14,5 +14,6 @@ builder.Services.AddBlazorStrap();
 builder.Services.AddScoped(sp => new BSOffCanvas { });
 builder.Services.AddScoped(sp => new InfoModal { });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 
 await builder.Build().RunAsync();
